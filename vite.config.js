cd7ee95d7b47cbc7438eab95602c080e3731f0b4
BayanMuaddel <bayanmouddil@gmail.com>
2026-08-03 16:55:41 +0300

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
+  base: "/weather-app/",
+
   plugins: [
     vue(),
     vueDevTools(),
@@ -12,9 +14,7 @@ export default defineConfig({
 
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-
-  base: '/weather-app/',
-})
+});
